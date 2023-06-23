cf48dfd6e8cc39b5ec25e1497b6e4700123fc460
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-23 16:41:16 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    {id: 3, description: "Charger", quantity: 1, packed: false}
+  ];
 
 export default function App() {
     return (
@@ -26,15 +30,29 @@ function Form() {
 function PackingList() { 
     return (
         <div className="list">
-            List
+        <ul>
+            {initialItems.map(item=><Item item={item} />)}
+        </ul>
         </div>
     )
 }
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    )
+}
+
 function Stats() {
     return (
         <footer className="stats">
             <em>
-                💼 You have x items on your list and you already packed x
+                💼 You have x items on your list and you already packed 
             </em>
         </footer>
     )
